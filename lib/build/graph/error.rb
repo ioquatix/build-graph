b62640e89a777cba05b4766b18673b386e102fa2
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2014-06-18 09:38:46 +1200

    Fixed CommandFailure scope.

--- lib/build/graph/error.rb
+++ lib/build/graph/error.rb
@@ -19,18 +19,20 @@
 # THE SOFTWARE.
 
 module Build
-	class TransientError < StandardError
-	end
-	
-	class CommandFailure < TransientError
-		def initialize(command, status)
-			super "Command #{command.inspect} failed with exit status #{status}!"
-		
-			@command = command
-			@status = status
+	module Graph
+		class TransientError < StandardError
 		end
 	
-		attr :command
-		attr :status
+		class CommandFailure < TransientError
+			def initialize(command, status)
+				super "Command #{command.inspect} failed with exit status #{status}!"
+		
+				@command = command
+				@status = status
+			end
+	
+			attr :command
+			attr :status
+		end
 	end
 end
